b04a8c45d415c5c420b74fe0fd6d521cd0eab53a
Lennart Fridén <lennart@magplus.com>
2014-04-09 11:33:01 +0200

    Bump version to 0.4.0
    Closes #5

--- spec/login_cookie/cookie_parser_spec.rb
+++ spec/login_cookie/cookie_parser_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe LoginCookie::CookieParser do
   let(:time) { Time.parse('2001-01-01 00:00:00 UTC').utc }
+  let(:payload_valid_in_2001) { "eyJlbWFpbCI6InRlc3QiLCJ2ZXJzaW9uIjoiMC40LjAiLCJleHBpcmVzX2F0\nIjoiMjAwMS0wMS0yMiAwMDowMDowMCBVVEMiLCJ2ZXJzaW9uIjoiMC40LjAi\nLCJleHBpcmVzX2F0IjoiMjAwMS0wMS0yMiAwMDowMDowMCBVVEMifQ==\n.95ab62cefdc3e9f392dee50518741608acf01f53fa0df8b758bb4f73d459305f" }
 
   before { Timecop.freeze(time) }
   after { Timecop.return }
 
   describe '.parse' do
     it 'returns a hash of the cookie payload' do
-      payload = "eyJlbWFpbCI6InRlc3QiLCJ2ZXJzaW9uIjoiMC4zLjAiLCJleHBpcmVzX2F0\nIjoiMjAwMS0wMS0yMiAwMDowMDowMCBVVEMifQ==\n.3cf283f1c92ffe0ae8ebe2eef686d83a90a46c853a33bd52354d01d19ef79c8c"
-      expected_content = { 'email' => 'test', 'version' => LoginCookie::VERSION, 'expires_at' => (time+LoginCookie.config.ttl).to_s }
-      expect(LoginCookie::CookieParser.parse(payload)).to eq expected_content
+      expected_content = { 'email' => 'test', 'version' => LoginCookie::VERSION, 'expires_at' => (time + LoginCookie.config.ttl).to_s }
+      expect(LoginCookie::CookieParser.parse(payload_valid_in_2001)).to eq expected_content
     end
 
-    it 'raises an error if the payload digest doesnt match the content' do
-      expect { LoginCookie::CookieParser.parse("e30K.asd") }.to raise_error(LoginCookie::InvalidDigest)
+    it 'raises an error if the payload digest does not match the content' do
+      expect { LoginCookie::CookieParser.parse('e30K.asd') }.to raise_error(LoginCookie::InvalidDigest)
     end
 
     it 'raises an error if the payload was generated with another version' do
@@ -24,8 +24,7 @@ describe LoginCookie::CookieParser do
 
     it 'raises an error if the payload has expired' do
       Timecop.return
-      error_expired = "eyJlbWFpbCI6InRlc3QiLCJ2ZXJzaW9uIjoiMC4zLjAiLCJleHBpcmVzX2F0\nIjoiMjAwMS0wMS0yMSAyMzowMDowMCBVVEMifQ==\n.feb4e88ac61f1faaf4ef078350e340339e9316d591bf043e600b597f890990b0"
-      expect{ LoginCookie::CookieParser.parse(error_expired) }.to raise_error(LoginCookie::ExpiredCookie)
+      expect { LoginCookie::CookieParser.parse(payload_valid_in_2001) }.to raise_error(LoginCookie::ExpiredCookie)
     end
   end
 end
